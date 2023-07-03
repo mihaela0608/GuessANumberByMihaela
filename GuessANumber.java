557dda19fa38f07532e6b51d2da07cbf0a99a502
Mihaela <mihaelabataklieva2006@gmail.com>
2023-07-03 11:00:08 +0300

    Uploaded my first project

---
+++ GuessANumber.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessANumber {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        Random randomNumber=new Random();
+        int computerNumber= randomNumber.nextInt(101);
+        while(true){
+            System.out.print("Guess a number (1-100): ");
+            String playerInput=scanner.nextLine();
+            int playerNumber;
+            boolean isValid=true;
+            for (int i = 0; i < playerInput.length(); i++) {
+                if (playerInput.charAt(i) < 48 || playerInput.charAt(i) > 57){
+                isValid=false;
+                break;
+                }
+            }
+            if (isValid){
+                playerNumber=Integer.parseInt(playerInput);
+                if (playerNumber == computerNumber){
+                    System.out.println("You guessed it!");
+                    break;
+                } else if (playerNumber< computerNumber) {
+                    System.out.println("Too Low");
+                } else  {
+                    System.out.println("Too High");
+                }
+            } else{
+                System.out.println("Invalid input.");
+            }
+        }
+
+    }
+}
