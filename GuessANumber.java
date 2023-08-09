2c00ab875bcde92a9f1ba10f89a03265be947acc
Mihaela Bataklieva <133794226+mihaela0608@users.noreply.github.com>
2023-08-09 11:03:52 +0300

    Update GuessANumber.java

--- GuessANumber.java
+++ GuessANumber.java
@@ -1,3 +1,5 @@
+package GuessANumberByMihaela;
+
 import java.util.Random;
 import java.util.Scanner;
 
@@ -6,6 +8,7 @@ public class GuessANumber {
         Scanner scanner=new Scanner(System.in);
         Random randomNumber=new Random();
         int computerNumber= randomNumber.nextInt(101);
+        int count=0;
         while(true){
             System.out.print("Guess a number (1-100): ");
             String playerInput=scanner.nextLine();
@@ -21,11 +24,14 @@ public class GuessANumber {
                 playerNumber=Integer.parseInt(playerInput);
                 if (playerNumber == computerNumber){
                     System.out.println("You guessed it!");
+                    System.out.printf("You guess the number in %d tries",count+1);
                     break;
                 } else if (playerNumber< computerNumber) {
                     System.out.println("Too Low");
+                    count++;
                 } else  {
                     System.out.println("Too High");
+                    count++;
                 }
             } else{
                 System.out.println("Invalid input.");
